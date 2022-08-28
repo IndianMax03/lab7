7dad2b2c8dc5c8f8076c21f833281d8ed8b82e67
IndianMax <58441613+IndianMax03@users.noreply.github.com>
2022-08-29 01:04:16 +0300

    ClientInvoker class has been upgraded

--- Client/src/main/java/client/ClientInvoker.java
+++ Client/src/main/java/client/ClientInvoker.java
@@ -1,10 +1,12 @@
 package client;
 
+import command.CommandsEnum;
 import commands.*;
 import listening.Request;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class ClientInvoker {
 
@@ -12,21 +14,11 @@ public class ClientInvoker {
 
     public ClientInvoker(ClientReceiver clientReceiver) {
 
-        commandMap.put("add", new Add(clientReceiver));
-        commandMap.put("add_if_min", new AddIfMin(clientReceiver));
-        commandMap.put("clear", new Clear());
-        commandMap.put("execute_script", new ExecuteScript());
-        commandMap.put("exit", new Exit());
-        commandMap.put("filter_starts_with_name", new FilterStartsWithName());
-        commandMap.put("help", new Help());
-        commandMap.put("info", new Info());
-        commandMap.put("print_descending", new PrintDescending());
-        commandMap.put("remove_all_by_government", new RemoveAllByGovernment(clientReceiver));
-        commandMap.put("remove_by_id", new RemoveById(clientReceiver));
-        commandMap.put("remove_greater", new RemoveGreater(clientReceiver));
-        commandMap.put("remove_lower", new RemoveLower(clientReceiver));
-        commandMap.put("show", new Show());
-        commandMap.put("update", new Update(clientReceiver));
+        for (CommandsEnum command : CommandsEnum.values()) {
+            Optional<ClientCommand> optional = create(clientReceiver, command);
+            optional.ifPresent(clientCommand -> commandMap.put(command.title, clientCommand));
+            System.out.println(commandMap);
+        }
     }
 
     public Request check(String commandName, String argument) {
@@ -34,6 +26,44 @@ public class ClientInvoker {
             return this.commandMap.get(commandName).execute(argument);
         System.out.println("Введённой команды не существует.");
         return null;
+    }
+
+    private Optional<ClientCommand> create(ClientReceiver clientReceiver, CommandsEnum command) {
+        switch (command) {
+        case ADD:
+            return Optional.of(new Add(clientReceiver));
+        case ADD_IF_MIN:
+            return Optional.of(new AddIfMin(clientReceiver));
+        case CLEAR:
+            return Optional.of(new Clear());
+        case EXECUTE_SCRIPT:
+            return Optional.of(new ExecuteScript());
+        case EXIT:
+            return Optional.of(new Exit());
+        case FILTER_STARTS_WITH_NAME:
+            return Optional.of(new FilterStartsWithName());
+        case HELP:
+            return Optional.of(new Help());
+        case INFO:
+            return Optional.of(new Info());
+        case PRINT_DESCENDING:
+            return Optional.of(new PrintDescending());
+        case REMOVE_ALL_BY_GOVERNMENT:
+            return Optional.of(new RemoveAllByGovernment(clientReceiver));
+        case REMOVE_BY_ID:
+            return Optional.of(new RemoveById(clientReceiver));
+        case REMOVE_GREATER:
+            return Optional.of(new RemoveGreater(clientReceiver));
+        case REMOVE_LOWER:
+            return Optional.of(new RemoveLower(clientReceiver));
+        case SHOW:
+            return Optional.of(new Show());
+        case UPDATE:
+            return Optional.of(new Update(clientReceiver));
+        case AUTHORIZATION:
+        default:
+            return Optional.empty();
+        }
     }
 
 }
