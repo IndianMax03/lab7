898a1a137f1743cab54716dc0c55c3b3c0df7a0e
IndianMax <58441613+IndianMax03@users.noreply.github.com>
2022-08-30 23:46:13 +0300

    formatted ServerInvoker class

--- Server/src/main/java/server/ServerInvoker.java
+++ Server/src/main/java/server/ServerInvoker.java
@@ -26,8 +26,9 @@ public class ServerInvoker {
 
     public Optional<Response> execute(Request request) {
         String commandName = request.getCommandName();
-        if (request.getLogin().equals("") && !commandName.equals("authorization")){
-            return Optional.of(new Response("Выполнение команд не доступно неавторизованным пользователям.\nВведите authorization, чтобы зарегестрироваться в системе."));
+        if (request.getLogin().equals("") && !commandName.equals("authorization")) {
+            return Optional.of(new Response(
+                    "Выполнение команд не доступно неавторизованным пользователям.\nВведите authorization, чтобы зарегестрироваться в системе."));
         }
 
         return this.commandMap.get(commandName).execute(request);
