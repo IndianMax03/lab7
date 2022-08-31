45978fd7a47a7162828df928c2ec43804c2bcec2
IndianMax <58441613+IndianMax03@users.noreply.github.com>
2022-08-31 04:23:36 +0300

    added better defence in ServerInvoker class logic

--- Server/src/main/java/server/ServerInvoker.java
+++ Server/src/main/java/server/ServerInvoker.java
@@ -26,7 +26,7 @@ public class ServerInvoker {
 
     public Optional<Response> execute(Request request) {
         String commandName = request.getCommandName();
-        if (request.getLogin().equals("") && !commandName.equals("authorization")) {
+        if (request.getLogin() == null || request.getPassword() == null || request.getLogin().equals("") && !commandName.equals("authorization")) {
             return Optional.of(new Response(
                     "Выполнение команд не доступно неавторизованным пользователям.\nВведите authorization, чтобы зарегестрироваться в системе."));
         }
