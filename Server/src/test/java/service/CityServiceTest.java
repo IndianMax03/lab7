295061c2d183498304279bd48ec545486d364747
IndianMax <58441613+IndianMax03@users.noreply.github.com>
2022-09-04 05:17:11 +0300

    City Service tests was simplified

--- Server/src/test/java/service/CityServiceTest.java
+++ Server/src/test/java/service/CityServiceTest.java
@@ -1,7 +1,11 @@
 package service;
 
 import base.*;
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Assert;
+import static org.junit.Assert.*;
 
 import java.util.SortedSet;
 
@@ -47,89 +51,77 @@ public class CityServiceTest {
     public void testUpdateByIdWithValidId() {
         int id = cityService.create(city, "test");
         city.setName("updateTest");
-        Boolean result = cityService.updateById(id, city, "test");
-        Assert.assertEquals(result, true);
+        assertTrue(cityService.updateById(id, city, "test"));
     }
 
     @Test
     public void testUpdateByIdWithInvalidId() {
         cityService.create(city, "test");
         city.setName("updateTest");
-        Boolean result = cityService.updateById(-1, city, "test");
-        Assert.assertEquals(result, false);
+        assertFalse(cityService.updateById(-1, city, "test"));
     }
 
     @Test
     public void testUpdateByIdWithFalseLogin() {
         int id = cityService.create(city, "test");
         city.setName("updateTest");
-        Boolean result = cityService.updateById(id, city, "xxx");
-        Assert.assertEquals(result, false);
+        assertFalse(cityService.updateById(id, city, "xxx"));
     }
 
     @Test
     public void testRemoveByIdWithValidId() {
         int id = cityService.create(city, "test");
-        Boolean result = cityService.removeById(id, "test");
-        Assert.assertEquals(result, true);
+        assertTrue(cityService.removeById(id, "test"));
     }
 
     @Test
     public void testRemoveByIdWithInvalidId() {
         cityService.create(city, "test");
-        Boolean result = cityService.removeById(-1, "test");
-        Assert.assertEquals(result, false);
+        assertFalse(cityService.removeById(-1, "test"));
     }
 
     @Test
     public void testRemoveByIdWithFalseLogin() {
         int id = cityService.create(city, "test");
-        Boolean result = cityService.removeById(id, "xxx");
-        Assert.assertEquals(result, false);
+        assertFalse(cityService.removeById(id, "xxx"));
     }
 
     @Test
     public void testClearByUserWithValidLogin() {
         cityService.create(city, "test");
-        Boolean result = cityService.clearByUser("test");
-        Assert.assertEquals(result, true);
+        assertTrue(cityService.clearByUser("test"));
     }
 
     @Test
     public void testClearByUserWithInvalidLogin() {
         cityService.create(city, "test");
-        Boolean result = cityService.clearByUser("xxx");
-        Assert.assertEquals(result, false);
+        assertFalse(cityService.clearByUser("xxx"));
     }
 
     @Test
     public void testRemoveAllByGovernmentWithExistingCity() {
         city.setGovernment(Government.ANARCHY);
         cityService.create(city, "test");
-        Boolean result = cityService.removeAllByGovernment(Government.ANARCHY.toString(), "test");
-        Assert.assertEquals(result, true);
+        assertTrue(cityService.removeAllByGovernment(Government.ANARCHY.toString(), "test"));
     }
 
     @Test
     public void testRemoveAllByGovernmentWithNotExistingCity() {
-        Boolean result = cityService.removeAllByGovernment(Government.ANARCHY.toString(), "test");
-        Assert.assertEquals(result, false);
+        assertFalse(cityService.removeAllByGovernment(Government.ANARCHY.toString(), "test"));
     }
 
     @Test
     public void testRemoveAllByGovernmentWithFalseArgument() {
         city.setGovernment(Government.ANARCHY);
         cityService.create(city, "test");
-        Boolean result = cityService.removeAllByGovernment(Government.DICTATORSHIP.toString(), "test");
-        Assert.assertEquals(result, false);
+        assertFalse(cityService.removeAllByGovernment(Government.DICTATORSHIP.toString(), "test"));
     }
 
     @Test
     public void testRemoveAllByGovernmentWithFalseLogin() {
         city.setGovernment(Government.ANARCHY);
         cityService.create(city, "test");
-        Boolean result = cityService.removeAllByGovernment(Government.ANARCHY.toString(), "xxx");
-        Assert.assertEquals(result, false);
+        assertFalse(cityService.removeAllByGovernment(Government.ANARCHY.toString(), "xxx"));
     }
 
     @Test
@@ -137,8 +129,7 @@ public class CityServiceTest {
         city.setName("xyz");
         cityService.create(city, "test");
         city.setName("abc");
-        Boolean result = cityService.removeGreater(city, "test");
-        Assert.assertEquals(result, true); // cause xyz > abc
+        assertTrue(cityService.removeGreater(city, "test")); // cause xyz > abc
     }
 
     @Test
@@ -146,8 +137,7 @@ public class CityServiceTest {
         city.setName("abc");
         cityService.create(city, "test");
         city.setName("xyz");
-        Boolean result = cityService.removeGreater(city, "test");
-        Assert.assertEquals(result, false); // cause abc < xyz
+        assertFalse(cityService.removeGreater(city, "test")); // cause abc < xyz
     }
 
     @Test
@@ -155,8 +145,7 @@ public class CityServiceTest {
         city.setName("abc");
         cityService.create(city, "test");
         city.setName("xyz");
-        Boolean result = cityService.removeGreater(city, "xxx");
-        Assert.assertEquals(result, false);
+        assertFalse(cityService.removeGreater(city, "xxx"));
     }
 
     @Test
@@ -164,8 +153,7 @@ public class CityServiceTest {
         city.setName("abc");
         cityService.create(city, "test");
         city.setName("xyz");
-        Boolean result = cityService.removeLower(city, "test");
-        Assert.assertEquals(result, true); // cause abc < xyz
+        assertTrue(cityService.removeLower(city, "test")); // cause abc < xyz
     }
 
     @Test
@@ -173,8 +161,7 @@ public class CityServiceTest {
         city.setName("xyz");
         cityService.create(city, "test");
         city.setName("abc");
-        Boolean result = cityService.removeLower(city, "test");
-        Assert.assertEquals(result, false); // cause xyz > abc
+        assertFalse(cityService.removeLower(city, "test")); // cause xyz > abc
     }
 
     @Test
@@ -182,7 +169,6 @@ public class CityServiceTest {
         city.setName("abc");
         cityService.create(city, "test");
         city.setName("xyz");
-        Boolean result = cityService.removeLower(city, "xxx");
-        Assert.assertEquals(result, false);
+        assertFalse(cityService.removeLower(city, "xxx"));
     }
 }
